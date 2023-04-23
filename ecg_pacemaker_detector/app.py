3385888c8a27eda34c3f4b4b9585ec993dd57a13
sagi-ezri <sagi.ezri@gmail.com>
2023-04-23 22:02:41 +0300

    rearrange code

--- ecg_pacemaker_detector/app.py
+++ ecg_pacemaker_detector/app.py
@@ -1,6 +1,6 @@
 import numpy as np
 import wfdb
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import keras
 import datetime
 import json
@@ -8,92 +8,78 @@ import os
 
 app = Flask(__name__)
 
-
-# load model using keras
-def load_model():
-    global model
-    model = keras.models.load_model("./model")
+# Load model using keras
+model = keras.models.load_model('./model/best')
 
 
-# welcome app
-@app.route("/")
+@app.route('/')
 def home_endpoint():
-    return "Welcome to the ECG Pacemaker Classifier - Sagi Ezri"
+    """Welcome app"""
+    return 'Welcome to the ECG Pacemaker Classifier - Sagi Ezri'
 
 
-# predict app
-@app.route("/predict", methods=["POST"])
+@app.route('/predict', methods=['POST'])
 def get_prediction():
-    if request.method == "POST":
-        age: int
-        weight: float
-        height: float
-        nurse: int
-        site: int
-        device: str
-        sex: int
-        patient_id: str
-        # get three files - metadata txt, signal data, signal header
-        files = request.files.getlist("files")
-        # get data posted as a json
-        request_data = json.load(files[0])
-        # get metadata
-        if request_data:
-            if "recording_date" in request_data:
-                recording_date = datetime.datetime.strptime(
-                    request_data["recording_date"], "%d/%m/%Y %H:%M:%S"
-                )
-            else:
-                return "Error: please recording date"
-            if "age" in request_data:
-                age = int(request_data["age"])
-            else:
-                return "Error: please insert age"
-            if "height" in request_data:
-                height = float(request_data["height"])
-            if "weight" in request_data:
-                weight = float(request_data["weight"])
-            if "nurse" in request_data:
-                nurse = int(request_data["nurse"])
-            if "site" in request_data:
-                site = int(request_data["site"])
-            if "device" in request_data:
-                device = str(request_data["device"])
-            if "sex" in request_data:
-                sex = str(request_data["sex"])
-            if "patient_id" in request_data:
-                patient_id = str(request_data["patient_id"])
-        # get the signal/header files
-        request_file_data = files[1]
-        request_file_header = files[2]
-        # send the wfdb the filename without extension
-        filename_without_ex = os.path.splitext(request_file_header.filename)[0]
-        # save the uploaded files
-        request_file_data.save(request_file_data.filename)
-        request_file_header.save(request_file_header.filename)
-        if request_file_data and request_file_header:
-            # load the data into vector array
-            raw_data = wfdb.rdsamp(filename_without_ex)
-            ecg_signal_data = raw_data[0]
-            # reshape to match the model dims
-            ecg_signal_data = ecg_signal_data.reshape(
-                1, ecg_signal_data.shape[0], ecg_signal_data.shape[1], 1
-            )
-        else:
-            return "Error: please load a .dat and .hea files"
-        if age > 18:
-            # runs globally loaded model on the data
-            prediction = model.predict(ecg_signal_data)
-            pacemaker_bool_result = bool(np.argmax(prediction[0]))
-            return "Pacemakr result: {}, Age: {}, Scan Date: {}".format(
-                pacemaker_bool_result, age, recording_date
-            )
-        else:
-            return "The patient is below 18 - Pacemakr result: {}, Age: {}, Scan Date: {}".format(
-                None, age, recording_date
-            )
+    """Receive files and predict pacemaker"""
+    # Get data
+    age = request.json.get('age')
+    weight = request.json.get('weight')
+    height = request.json.get('height')
+    nurse = request.json.get('nurse')
+    site = request.json.get('site')
+    device = request.json.get('device')
+    sex = request.json.get('sex')
+    patient_id = request.json.get('patient_id')
+
+    # Get files
+    files = request.files.getlist('files')
+    metadata = json.load(files[0])
+    signal = files[1].filename
+    header = files[2].filename
+
+    # Parse metadata
+    if 'recording_date' in metadata:
+        recording_date = datetime.datetime.strptime(metadata['recording_date'],
+                                                     '%d/%m/%Y %H:%M:%S')
+    else:
+        return {'error': 'Please provide recording date'}
+    if 'age' not in metadata:
+        return {'error': 'Please provide age'}
+    if 'height' in metadata:
+        height = float(height)
+    if 'weight' in metadata:
+        weight = float(weight)
+    if 'nurse' in metadata:
+        nurse = int(nurse)
+    if 'site' in metadata:
+        site = int(site)
+    if 'device' in metadata:
+        device = str(device)
+    if 'sex' in metadata:
+        sex = str(sex)
+    if 'patient_id' in metadata:
+        patient_id = str(patient_id)
+
+    # Load data into vector array
+    filename_without_ex = os.path.splitext(header)[0]
+    data, _ = wfdb.rdsamp(filename_without_ex)
+    ecg_signal_data = data.reshape((1,) + data.shape + (1,))
+
+    # Run model
+    prediction = model.predict(ecg_signal_data)
+    pacemaker_result = bool(np.argmax(prediction[0]))
+
+    # Format output
+    if age > 18:
+        output = {'pacemaker_result': pacemaker_result,
+                  'age': age,
+                  'recording_date': str(recording_date)}
+    else:
+        output = {'pacemaker_result': None,
+                  'age': age,
+                  'recording_date': str(recording_date)}
+    return output
 
 
-def start() -> None:
-    load_model()
-    app.run(host="0.0.0.0", port=3000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
